34e1e4b93b6785a980dcd8c07e9090e53f4e9c49
Your Name <muhammadakmalbwn2019@gmail.com>
2026-02-23 09:10:07 -0800

    some mergings

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+ 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -19,28 +19,5 @@ export default defineConfig({
         secure: false,
       }
     }
-  }
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://wewave-backend.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/auth': {
-        target: 'https://wewave-backend.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
->>>>>>> befc4f5e4500ad9817c51883bab2430cbc28f956
+  } 
 })
